a14bb0248f56738f797f04365ca0163ce8b6d186
Le Dung <ledung2841998@gmail.com>
2020-12-14 16:21:24 +0700

    change

--- Server/pictureImpl.java
+++ Server/pictureImpl.java
@@ -14,6 +14,7 @@ public class pictureImpl extends UnicastRemoteObject implements IPicture  {
     public String drawPic(String data) throws RemoteException {
         dataHandling dh = new dataHandling();
         invertColor ic = new invertColor();
+        edgeDetector ed = new edgeDetector();
 
         BufferedImage img=null;
         try {
@@ -23,16 +24,16 @@ public class pictureImpl extends UnicastRemoteObject implements IPicture  {
         }
         CannyEdgeDetector detector = new CannyEdgeDetector();
         //set thresdhold of low and high
-        detector.setLowThreshold(1.0f);
-        detector.setHighThreshold(5.0f);
+       // detector.setLowThreshold(1.0f);
+       // detector.setHighThreshold(5.0f);
 
         // split to get the edge
-        detector.setSourceImage(img);
-        detector.process();
-        BufferedImage edges = detector.getEdgesImage();
-
+  //      detector.setSourceImage(img);
+  //      detector.process();
+//        BufferedImage edges = detector.getEdgesImage();
+        BufferedImage edges = ed.edgeDectect(img);
         // convert the edge to binaryimage to get better line
-       BufferedImage invertEdge = ic.invertedColor(edges);
+     //  BufferedImage invertEdge = ic.invertedColor(edges);
         // get color from origon pic to coloring
         Segmentize sm = new Segmentize(img,40);
         BufferedImage colors = sm.segmentize();
@@ -42,12 +43,12 @@ public class pictureImpl extends UnicastRemoteObject implements IPicture  {
 
         for (int i = 1; i < x - 1; i++) {
             for (int j = 1; j < y - 1; j++) {
-                int val = invertEdge.getRGB(i, j);
+                int val = edges.getRGB(i, j);
                 int r = (0x00ff0000 & val) >> 16;
                 int g = (0x0000ff00 & val) >> 8;
                 int b = (0x000000ff & val);
                 int m=(r+g+b);
-                if(m < 675)
+                if(m < 685)
                 {
                     colors.setRGB(i, j, 0);
                 }
